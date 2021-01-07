824c0962b377c5c1e39ece5ac491094a3e6ee4df
daftmaple <ubuntumaple@gmail.com>
2021-01-07 19:01:22 +1100

    Updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,24 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 11,
+    ecmaVersion: 6,
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react', '@typescript-eslint', 'jsx-a11y'],
   rules: {
     '@typescript-eslint/no-empty-interface': 0,
     '@typescript-eslint/no-empty-function': 0,
